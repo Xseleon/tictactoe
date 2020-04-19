bf17ad6679fa898c2a78ac87b847c5306e39be46
Xseleon <xseleon.ratheos@gmail.com>
2020-04-19 10:49:03 -0400

    adding tictactoe file to github

---
+++ tictactoe.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+
+//Make a game of tic tac toe
+/*
+
+Need a board class
+
+Need a way to enter in inputs
+
+Play against another player or a computer
+
+Run in a main program
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+int main() {
+
+    //build a board and have it populated in a 2D array
+
+
+   
+
+
+    return 0;
+}
+
+
+
+
